8f709f338e632e7c2e4e0f046105025cc812c339
petermc87 <peter.mcgibney@gmail.com>
2024-01-25 17:10:38 -0500

    feat: create button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "react-bootstrap";
 
+import styles from "./page.module.scss";
+
 export default function Home() {
   return (
     <div>
-      <Button>This Button</Button>
+      <Button id={styles.loginButtonEl}>This Button</Button>
     </div>
   );
 }
