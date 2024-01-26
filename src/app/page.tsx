b17d56889cc95e81b0cec8bce273f238ddfc5371
petermc87 <peter.mcgibney@gmail.com>
2024-01-26 14:10:24 -0500

    styles: styling text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,9 @@ export default function Home() {
   return (
     <div>
       <Button id={styles.loginButtonEl}>This Button</Button>
+      <h1>Hello</h1>
+      <h2>Hello</h2>
+      <p>Hello</p>
     </div>
   );
 }
