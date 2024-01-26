ab02b411977923b398b71754eac491577c0b7559
petermc87 <peter.mcgibney@gmail.com>
2024-01-26 14:16:20 -0500

    chore: add Logo component template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Button } from "react-bootstrap";
 
+import Logo from "./components/Logo/logo";
 import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <div>
       <Button id={styles.loginButtonEl}>This Button</Button>
+      <Logo />
       <h1>Hello</h1>
       <h2>Hello</h2>
       <p>Hello</p>
