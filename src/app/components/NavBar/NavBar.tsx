aa4e313251d5371e84275d00475fa83176082c1d
petermc87 <peter.mcgibney@gmail.com>
2024-01-26 21:43:48 -0500

    feat: navbar component

---
+++ src/app/components/NavBar/NavBar.tsx
@@ -0,0 +1,12 @@
+import LoginButtonSignUpButton from "../LoginSignupButton/LoginSignupButtonModal";
+import Logo from "../Logo/logo";
+import styles from "./NavBar.module.scss";
+
+export default function NavBar() {
+  return (
+    <div className={styles.NavWrapper}>
+      <Logo />
+      <LoginButtonSignUpButton buttonChoice={false} />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import LoginSignupLarge from "./components/LoginSignupButton/LoginSignupButtonLarge";
-import LoginButtonSignUpButton from "./components/LoginSignupButton/LoginSignupButtonModal";
-import Logo from "./components/Logo/logo";
+import NavBar from "./components/NavBar/NavBar";
 import TitleText from "./components/TitleText/TitleText";
 
 export default function Home() {
   return (
     <div>
-      <LoginButtonSignUpButton buttonChoice={true} />
-      <LoginSignupLarge buttonChoice={false} />
-      <Logo />
+      <NavBar />
+      {/* <div></div> */}
       <TitleText text="An easy way to track your job applications" />
       <h1>Hello</h1>
       <h2>Hello</h2>
