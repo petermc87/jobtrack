45c02eb016174bbd65aaeb4234d3beecc57e7a03
petermc87 <peter.mcgibney@gmail.com>
2024-01-30 13:29:47 -0500

    style: input component

--- src/app/components/InputField/InputField.tsx
+++ src/app/components/InputField/InputField.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import { Form } from "react-bootstrap";
+import styles from "./InputField.module.scss";
 
 type InputTypes = {
   placeholderText: string;
 };
 
 export default function InputField({ placeholderText }: InputTypes) {
-  return <Form.Control type="text" placeholder={placeholderText} />;
+  return (
+    <Form.Control
+      type="text"
+      placeholder={placeholderText}
+      id={styles.inputWrapper}
+    />
+  );
 }
