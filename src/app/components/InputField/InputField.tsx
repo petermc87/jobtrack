3fedfec4d94fb26d7406b88837fcf304cc2d855e
petermc87 <peter.mcgibney@gmail.com>
2024-02-01 21:31:47 -0500

    feat: toggle password visibility

--- src/app/components/InputField/InputField.tsx
+++ src/app/components/InputField/InputField.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { Form } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Form, InputGroup } from "react-bootstrap";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
 import styles from "./InputField.module.scss";
 
 type InputTypes = {
@@ -8,11 +10,30 @@ type InputTypes = {
 };
 
 export default function InputField({ placeholderText }: InputTypes) {
+  // Use state to toggle the type.
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  // Changing the state on click.
+  const handleToggle = () => {
+    if (showPassword) setShowPassword(false);
+    else setShowPassword(true);
+  };
   return (
-    <Form.Control
-      type="text"
-      placeholder={placeholderText}
-      id={styles.inputWrapper}
-    />
+    <>
+      <InputGroup>
+        <Form.Control
+          type={showPassword ? "text" : "password"}
+          placeholder={placeholderText}
+          id={styles.inputWrapper}
+        />
+        {/* Eye button beside input to toggle pass visibility*/}
+        {placeholderText === "PASSWORD" && (
+          <Button onClick={() => handleToggle()}>
+            {/* If it is off(false), it is "password", and the opposite for "text" */}
+            {showPassword ? <FaRegEye /> : <FaRegEyeSlash />}
+          </Button>
+        )}
+      </InputGroup>
+    </>
   );
 }
