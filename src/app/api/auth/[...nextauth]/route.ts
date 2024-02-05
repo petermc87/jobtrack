7fb99bd8433f4ac6670230a2f854def44feb335b
petermc87 <peter.mcgibney@gmail.com>
2024-02-05 16:43:04 -0500

    refactor: next auth configurations

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,23 @@
 import bcrypt from "bcrypt";
-import { NextAuthOptions } from "next-auth";
+import { NextAuthOptions, User } from "next-auth";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import db from "../../../../../prisma/db";
+
+declare module "next-auth" {
+  interface User {
+    id: number;
+    password: string | undefined | number | null;
+    email: string | undefined | null;
+    username: string | undefined | null;
+  }
+  interface Session {
+    user: {
+      id: number | unknown;
+      username: string | unknown;
+    };
+  }
+}
 
 const authOptions: NextAuthOptions = {
   providers: [
@@ -40,6 +55,45 @@ const authOptions: NextAuthOptions = {
       },
     }),
   ],
+  callbacks: {
+    async session({ session, token }) {
+      session.user.id = token.id;
+      session.user.username = token.username;
+
+      // Add oauthStore.
+
+      return session;
+    },
+
+    // Populating the token with the current session
+    jwt({ token, account, trigger, session, user }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.id = user.id;
+        // Next auth User import??
+        token.username = (user as User).username;
+      }
+
+      // Updating the username and email in the front end
+      // if the user info is being updated.
+
+      if (trigger === "update" && session?.name) token.name = session.name;
+
+      if (trigger === "update" && session?.username)
+        token.username = session.username;
+
+      if (trigger === "update" && session?.email) token.email = session.email;
+
+      return token;
+    },
+  },
+  pages: {
+    signIn: "/",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.JWT_SESSION_SECRET,
 };
 
 const handler = NextAuth(authOptions);
