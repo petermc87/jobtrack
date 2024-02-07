e3d61aa46d3c23035c4f842c10d8d1408befdf81
petermc87 <peter.mcgibney@gmail.com>
2024-02-07 14:15:24 -0500

    feat: create typedefs matching prisma schema and user queries

---
+++ graphql/typeDefs.ts
@@ -0,0 +1,37 @@
+import gql from "graphql-tag";
+
+export const typeDefs = gql`
+  type User {
+    # ID! represents a non nullable id.
+    id: ID!
+    name: String
+    username: String
+    email: String
+    password: String
+    categories: [Category]
+  }
+
+  type Category {
+    id: ID!
+    name: String
+    # NOTE: The user @relation parameter is not matched here.
+    userId: String
+  }
+
+  type Job {
+    id: ID!
+    title: String
+    link: String
+    jobDescription: String
+    categoryId: String
+    resumeLink: String
+  }
+
+  #   GET requests.
+  type Query {
+    # Get one user
+    user(id: ID!): User
+    # Get all users
+    users: [Users]
+  }
+`;
