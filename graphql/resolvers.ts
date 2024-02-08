bd0374c9b69f67ab637bad162dcbe79647b26a48
petermc87 <peter.mcgibney@gmail.com>
2024-02-08 13:36:45 -0500

    feat: add get category

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -33,12 +33,25 @@ export const resolvers = {
         const categories = await context.prisma.category.findMany();
         return categories || [];
       } catch (error) {
-        console.error("Error when fetching category: ", error);
+        console.error("Error when fetching categories: ", error);
+        throw new Error("Unable to fetch categories");
+      }
+    },
+    category: async (parent: any, args: any, context: Context) => {
+      try {
+        const category = await context.prisma.category.findUnique({
+          where: {
+            id: args.id,
+          },
+        });
+        return category;
+      } catch (error) {
+        console.error("Error when fetching category", error);
         throw new Error("Unable to fetch category");
       }
     },
   },
-  // GET request --> for User to get the Categories contained within.
+  // User to get the Categories contained within. - GET
   User: {
     categories: async (parent: any, args: any, context: Context) => {
       try {
