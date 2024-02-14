aeea81da223cf334b54fdae1d47fd55674de7485
petermc87 <peter.mcgibney@gmail.com>
2024-02-14 16:22:02 -0500

    feat: viewing jobs placeholder

--- src/app/components/Categories/Categories.tsx
+++ src/app/components/Categories/Categories.tsx
@@ -1,9 +1,16 @@
 import { useQuery } from "@apollo/client";
-import { Category, User } from "@prisma/client";
+import { Job, User } from "@prisma/client";
 import { GET_USER } from "../../../../graphql/queries";
 
 type UserTypes = {
   user: User | undefined;
+};
+
+type CategoryTypes = {
+  id: any; // To resolve the Key types error in the categoryList return.
+  name: String;
+  userId: String;
+  jobs: [Job];
 };
 
 export default function Categories({ user }: UserTypes) {
@@ -15,12 +22,14 @@ export default function Categories({ user }: UserTypes) {
   if (error) return `Error: ${error}`;
 
   // List of categories to be returned
-  const categoriesList = data.user.categories.map((category: Category) => {
-    console.log(category);
+  const categoriesList = data.user.categories.map((category: CategoryTypes) => {
     return (
       <div key={category.id}>
         <h1>{category.name}</h1>
         <h2>JobList</h2>
+        {category.jobs?.map((job: Job) => {
+          return <p>{job.title}</p>;
+        })}
       </div>
     );
   });
