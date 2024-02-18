d564a61d65100344370922ba128b1387803fbe06
petermc87 <peter.mcgibney@gmail.com>
2024-02-18 10:32:58 -0500

    refactor: messages for no categories or jobs

--- src/app/components/Categories/Categories.tsx
+++ src/app/components/Categories/Categories.tsx
@@ -34,29 +34,37 @@ export default function Categories({ user }: UserTypes) {
   const categoriesList = data.user.categories.map((category: CategoryTypes) => {
     return (
       <div key={category.id}>
-        <h2>JobList</h2>
-        {category.jobs?.map((job: Job) => {
-          return (
-            <>
-              {/* If there are no categories, display: No categories to display`` */}
-              {!job ? (
-                <h2>No jobs to display</h2>
-              ) : (
-                <p key={job.id}>{job.title}</p>
-              )}
-            </>
-          );
-        })}
+        <h1>{category.name}</h1>
+
+        {/* Check if there are jobs contained within each category. If not, */}
+        {/* display a message. */}
+        {category.jobs.length ? (
+          <>
+            {category.jobs?.map((job: Job) => {
+              return (
+                <>
+                  <p key={job.id}>{job.title}</p>
+                </>
+              );
+            })}
+          </>
+        ) : (
+          <h2>No jobs to display</h2>
+        )}
       </div>
     );
   });
 
   return (
     <>
-      {data.user.categories.length() !== 0 ? (
-        <>{categoriesList && categoriesList}</>
+      {data && data.user.categories.length ? (
+        <>{categoriesList}</>
       ) : (
-        <h2>No categories to display</h2>
+        <>
+          <br />
+          <br />
+          <h2>No categories to display</h2>
+        </>
       )}
       {/* Returning the list of categories if the array within the user has contents. */}
     </>
