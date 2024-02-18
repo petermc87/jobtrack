c9615ebcdd00507a01b62feeb811f23c17579b15
petermc87 <peter.mcgibney@gmail.com>
2024-02-18 13:21:00 -0500

    chore: dry expandlargebutton component select function

--- src/app/components/ExpandButtons/ExpandButtonLarge.tsx
+++ src/app/components/ExpandButtons/ExpandButtonLarge.tsx
@@ -2,16 +2,9 @@ import { Dispatch, SetStateAction } from "react";
 import { FaCircleChevronDown, FaCircleChevronUp } from "react-icons/fa6";
 import styles from "./ExpandButtonLarge.module.scss";
 
-// type CategoryTypeDestructure = {
-//   id: string;
-//   name: String;
-//   userId: String;
-// };
-
 type ExpandButtonTypes = {
   showJobs: boolean;
   setShowJobs: Dispatch<SetStateAction<boolean>>;
-  // category: CategoryTypeDestructure;
   setCurrentSelected: Dispatch<SetStateAction<number>>;
   currentSelected: number;
   i: number;
@@ -23,64 +16,36 @@ export default function ExpandLarge({
   setCurrentSelected,
   currentSelected,
   i,
-}: // category,
-ExpandButtonTypes) {
+}: ExpandButtonTypes) {
+  // Function on click.
+  const handleSelectButton = () => {
+    if (showJobs) {
+      setShowJobs(false);
+    } else {
+      setShowJobs(true);
+    }
+    setCurrentSelected(i);
+  };
   return (
     <>
       {/* IF THE JOBS ARE NOT BEING SHOWN, AND IT IS CONCERNING THE CURRENT SELECTED CAT ONLY */}
       {!showJobs && currentSelected === i ? (
         <FaCircleChevronDown
           className={styles.buttonWrapper}
-          onClick={() => {
-            console.log("Click");
-            if (showJobs) {
-              setShowJobs(false);
-            } else {
-              setShowJobs(true);
-            }
-            setCurrentSelected(i);
-          }}
+          onClick={() => handleSelectButton()}
         />
       ) : // IF THE JOBS ARE BEING SHOWN, AND IT IS CONCERNING THE CURRENT SELECTED CAT ONLY
       showJobs && currentSelected === i ? (
         <FaCircleChevronUp
           className={styles.buttonWrapper}
-          onClick={() => {
-            if (showJobs) {
-              setShowJobs(false);
-            } else {
-              setShowJobs(true);
-            }
-            setCurrentSelected(i);
-          }}
+          onClick={() => handleSelectButton()}
         />
       ) : (
         <FaCircleChevronDown
           className={styles.buttonWrapper}
-          onClick={() => {
-            console.log("Click");
-            if (showJobs) {
-              setShowJobs(false);
-            } else {
-              setShowJobs(true);
-            }
-            setCurrentSelected(i);
-          }}
+          onClick={() => handleSelectButton()}
         />
       )}
-      {/* <Button
-        onClick={() => {
-          console.log("Click");
-          if (showJobs) {
-            setShowJobs(false);
-          } else {
-            setShowJobs(true);
-          }
-          setCurrentSelected(i);
-        }}
-      >
-        Click Me
-      </Button> */}
     </>
   );
 }
