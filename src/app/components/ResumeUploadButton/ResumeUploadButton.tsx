a73dfbf55c2dc9db3498284abc632e10430bb877
petermc87 <peter.mcgibney@gmail.com>
2024-02-24 13:21:31 -0500

    feat: add link redirect component

--- src/app/components/ResumeUploadButton/ResumeUploadButton.tsx
+++ src/app/components/ResumeUploadButton/ResumeUploadButton.tsx
@@ -1,4 +1,6 @@
 import { Job } from "@prisma/client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { Dispatch, SetStateAction, useState } from "react";
 import { Container, Form } from "react-bootstrap";
 import { MdPreview } from "react-icons/md";
@@ -20,12 +22,35 @@ export default function ResumeUpload({
   // Edit link state
   const [editLink, setEditLink] = useState(false);
 
+  const router = useRouter();
   return (
     <>
       <Container className={styles.buttonContainer}>
         {/* VIEW CONTAINER */}
         <Container className={styles.viewContainer}>
-          <MdPreview className={styles.buttonWrapper} id={styles.eye} />
+          {/* <MdPreview
+            className={styles.buttonWrapper}
+            id={styles.eye}
+            onClick={() => {
+              if (job) {
+                router.push(job.resumeLink);
+              }
+            }}
+          /> */}
+          <Link href={job.resumeLink} passHref>
+            <a target="_blank" rel="noopener noreferrer">
+              <MdPreview
+                className={styles.buttonWrapper}
+                id={styles.eye}
+                onClick={() => {
+                  if (job) {
+                    router.push(job.resumeLink);
+                  }
+                }}
+              />
+            </a>
+          </Link>
+
           <div
             className={styles.viewTextWrapper}
             onClick={() => {
